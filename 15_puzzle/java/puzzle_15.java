5eef3d7e7f65de6d1a9d8368dc26b61c29570cd9
Noah Overcash <smileytechguy@smileytechguy.com>
2016-12-29 13:24:13 -0500

    15 puzzle: shrink java, clean up, methodize

--- 15_puzzle/java/puzzle_15.java
+++ 15_puzzle/java/puzzle_15.java
@@ -4,23 +4,28 @@ import java.util.Random;
 
 public class puzzle_15 extends WindowController {
 	// set in stone
-	final private int ROWS = 4;
-	final private int COLS = 4;
+	final private int ROWS = 20;
+	final private int COLS = 20;
+	private int colWidth, rowWidth;
 
-	FilledRect[][] boxes = new FilledRect[ROWS][COLS];
-	FramedRect[][] boxFrames = new FramedRect[ROWS][COLS];
-	Text[][] boxText = new Text[ROWS][COLS];
-	int[][] boxVals = new int[ROWS][COLS];
+	final private Color grey=new Color(0xeeeeee), white=new Color(0xffffff);
 
-	int[] blankLoc = new int[2];
+	private FilledRect[][] boxes = new FilledRect[ROWS][COLS];
+	private FramedRect[][] boxFrames = new FramedRect[ROWS][COLS];
+	private Text[][] boxText = new Text[ROWS][COLS];
+	private int[][] boxVals = new int[ROWS][COLS];
+
+	private int[] blank = new int[2];
 
 	public static void main(String[] args) {
 		// objectdraw init
-		new puzzle_15().startController(512,512+44); // 44 is for correction from menu, want squares
+		new puzzle_15().startController(512,512+44); // 44 is correction
 	}
 
 	public void begin() {
 		// have to use this method bc objectdraw
+		colWidth = canvas.getWidth()/COLS;
+		rowWidth = canvas.getHeight()/ROWS;
 		shuffleVals();
 		initBoxes();
 	}
@@ -52,101 +57,61 @@ public class puzzle_15 extends WindowController {
 		for (int row=0; row<ROWS; row++) {
 			for (int col=0; col<COLS; col++) {
 				// make BG
-				boxes[row][col] = new FilledRect(canvas.getWidth()/COLS*col, canvas.getHeight()/ROWS*row, canvas.getWidth()/COLS, canvas.getHeight()/ROWS, canvas);
-				boxes[row][col].setColor(new Color(0xeeeeee));
+				boxes[row][col] = new FilledRect(colWidth*col, rowWidth*row, colWidth, rowWidth, canvas);
+				boxes[row][col].setColor(grey);
 
 				// make grid
-				boxFrames[row][col] = new FramedRect(canvas.getWidth()/COLS*col, canvas.getHeight()/ROWS*row, canvas.getWidth()/COLS, canvas.getHeight()/ROWS, canvas);
+				boxFrames[row][col] = new FramedRect(colWidth*col, rowWidth*row, colWidth, rowWidth, canvas);
 
 				// make nums
-				boxText[row][col] = new Text(boxVals[row][col], (canvas.getWidth()/COLS*col)+(canvas.getWidth()/COLS/2), (canvas.getHeight()/ROWS*row)+(canvas.getHeight()/ROWS/2), canvas);
-				boxText[row][col].setFontSize(canvas.getWidth()/COLS/2);
+				boxText[row][col] = new Text(boxVals[row][col], (colWidth*col)+(colWidth/2), (rowWidth*row)+(rowWidth/2), canvas);
+				boxText[row][col].setFontSize(colWidth/2);
 				boxText[row][col].move(-boxText[row][col].getWidth()/2, -boxText[row][col].getHeight()/2);
 
 				// 0 is our empty spot
 				if (boxVals[row][col] == 0) {
 					boxText[row][col].setText("");
-					blankLoc[0] = row;
-					blankLoc[1] = col;
-					boxes[row][col].setColor(new Color(0xffffff));
+					blank[0] = row;
+					blank[1] = col;
+					boxes[row][col].setColor(white);
 				}
 			}
 		}
 	}
 
 	public void onMouseClick(Location click) {
-		if (blankLoc[0] != 0 && boxes[blankLoc[0]-1][blankLoc[1]].contains(click)) { // check N box
-			// swap colors
-			boxes[blankLoc[0]-1][blankLoc[1]].setColor(new Color(0xffffff));
-			boxes[blankLoc[0]][blankLoc[1]].setColor(new Color(0xeeeeee));
-
-			// old blank becomes clicked num
-			boxText[blankLoc[0]][blankLoc[1]].setText(boxVals[blankLoc[0]-1][blankLoc[1]]);
-			boxText[blankLoc[0]][blankLoc[1]].moveTo((canvas.getWidth()/COLS*blankLoc[1])+(canvas.getWidth()/COLS/2), (canvas.getHeight()/ROWS*blankLoc[0])+(canvas.getHeight()/ROWS/2));
-			boxText[blankLoc[0]][blankLoc[1]].move(-boxText[blankLoc[0]][blankLoc[1]].getWidth()/2, -boxText[blankLoc[0]][blankLoc[1]].getHeight()/2);
-			boxText[blankLoc[0]-1][blankLoc[1]].setText("");
-
-			// update vals
-			boxVals[blankLoc[0]][blankLoc[1]] = boxVals[blankLoc[0]-1][blankLoc[1]];
-			boxVals[blankLoc[0]-1][blankLoc[1]] = 0;
-
-			// update blankLoc
-			blankLoc[0] -= 1;
-		} else if (blankLoc[1] != COLS-1 && boxes[blankLoc[0]][blankLoc[1]+1].contains(click)) { // check E box
-			// swap colors
-			boxes[blankLoc[0]][blankLoc[1]+1].setColor(new Color(0xffffff));
-			boxes[blankLoc[0]][blankLoc[1]].setColor(new Color(0xeeeeee));
-
-			// old blank becomes clicked num
-			boxText[blankLoc[0]][blankLoc[1]].setText(boxVals[blankLoc[0]][blankLoc[1]+1]);
-			boxText[blankLoc[0]][blankLoc[1]].moveTo((canvas.getWidth()/COLS*blankLoc[1])+(canvas.getWidth()/COLS/2), (canvas.getHeight()/ROWS*blankLoc[0])+(canvas.getHeight()/ROWS/2));
-			boxText[blankLoc[0]][blankLoc[1]].move(-boxText[blankLoc[0]][blankLoc[1]].getWidth()/2, -boxText[blankLoc[0]][blankLoc[1]].getHeight()/2);
-			boxText[blankLoc[0]][blankLoc[1]+1].setText("");
-
-			// update vals
-			boxVals[blankLoc[0]][blankLoc[1]] = boxVals[blankLoc[0]][blankLoc[1]+1];
-			boxVals[blankLoc[0]][blankLoc[1]+1] = 0;
-
-			// update blankLoc
-			blankLoc[1] += 1;
-		} else if (blankLoc[0] != ROWS-1 && boxes[blankLoc[0]+1][blankLoc[1]].contains(click)) { // check S box
-			// swap colors
-			boxes[blankLoc[0]+1][blankLoc[1]].setColor(new Color(0xffffff));
-			boxes[blankLoc[0]][blankLoc[1]].setColor(new Color(0xeeeeee));
-
-			// old blank becomes clicked num
-			boxText[blankLoc[0]][blankLoc[1]].setText(boxVals[blankLoc[0]+1][blankLoc[1]]);
-			boxText[blankLoc[0]][blankLoc[1]].moveTo((canvas.getWidth()/COLS*blankLoc[1])+(canvas.getWidth()/COLS/2), (canvas.getHeight()/ROWS*blankLoc[0])+(canvas.getHeight()/ROWS/2));
-			boxText[blankLoc[0]][blankLoc[1]].move(-boxText[blankLoc[0]][blankLoc[1]].getWidth()/2, -boxText[blankLoc[0]][blankLoc[1]].getHeight()/2);
-			boxText[blankLoc[0]+1][blankLoc[1]].setText("");
-
-			// update vals
-			boxVals[blankLoc[0]][blankLoc[1]] = boxVals[blankLoc[0]+1][blankLoc[1]];
-			boxVals[blankLoc[0]+1][blankLoc[1]] = 0;
-
-			// update blankLoc
-			blankLoc[0] += 1;
-		} else if (blankLoc[1] != 0 && boxes[blankLoc[0]][blankLoc[1]-1].contains(click)) { // check W box
-			// swap colors
-			boxes[blankLoc[0]][blankLoc[1]-1].setColor(new Color(0xffffff));
-			boxes[blankLoc[0]][blankLoc[1]].setColor(new Color(0xeeeeee));
-
-			// old blank becomes clicked num
-			boxText[blankLoc[0]][blankLoc[1]].setText(boxVals[blankLoc[0]][blankLoc[1]-1]);
-			boxText[blankLoc[0]][blankLoc[1]].moveTo((canvas.getWidth()/COLS*blankLoc[1])+(canvas.getWidth()/COLS/2), (canvas.getHeight()/ROWS*blankLoc[0])+(canvas.getHeight()/ROWS/2));
-			boxText[blankLoc[0]][blankLoc[1]].move(-boxText[blankLoc[0]][blankLoc[1]].getWidth()/2, -boxText[blankLoc[0]][blankLoc[1]].getHeight()/2);
-			boxText[blankLoc[0]][blankLoc[1]-1].setText("");
-
-			// update vals
-			boxVals[blankLoc[0]][blankLoc[1]] = boxVals[blankLoc[0]][blankLoc[1]-1];
-			boxVals[blankLoc[0]][blankLoc[1]-1] = 0;
-
-			// update blankLoc
-			blankLoc[1] -= 1;
+		if (blank[0] != 0 && boxes[blank[0]-1][blank[1]].contains(click)) { // check N box
+			moveWithOffset(-1,0);
+		} else if (blank[1] != COLS-1 && boxes[blank[0]][blank[1]+1].contains(click)) { // check E box
+			moveWithOffset(0,1);
+		} else if (blank[0] != ROWS-1 && boxes[blank[0]+1][blank[1]].contains(click)) { // check S box
+			moveWithOffset(1,0);
+		} else if (blank[1] != 0 && boxes[blank[0]][blank[1]-1].contains(click)) { // check W box
+			moveWithOffset(0,-1);
 		}
-		if (blankLoc[0] == COLS-1 && blankLoc[1] == ROWS-1) {
+		if (blank[0] == COLS-1 && blank[1] == ROWS-1) {
 			checkWinner();
 		}
+	}
+
+	public void moveWithOffset(int offsetX, int offsetY) {
+		// swap colors
+		boxes[blank[0]+offsetX][blank[1]+offsetY].setColor(white);
+		boxes[blank[0]][blank[1]].setColor(grey);
+
+		// old blank becomes clicked num
+		boxText[blank[0]][blank[1]].setText(boxVals[blank[0]+offsetX][blank[1]+offsetY]);
+		boxText[blank[0]][blank[1]].moveTo((colWidth*blank[1])+(colWidth/2), (rowWidth*blank[0])+(rowWidth/2));
+		boxText[blank[0]][blank[1]].move(-boxText[blank[0]][blank[1]].getWidth()/2, -boxText[blank[0]][blank[1]].getHeight()/2);
+		boxText[blank[0]+offsetX][blank[1]+offsetY].setText("");
+
+		// update vals
+		boxVals[blank[0]][blank[1]] = boxVals[blank[0]+offsetX][blank[1]+offsetY];
+		boxVals[blank[0]+offsetX][blank[1]+offsetY] = 0;
+
+		// update blank
+		blank[0] += offsetX;
+		blank[1] += offsetY;
 	}
 
 	public void checkWinner() {
