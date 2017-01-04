2d2766df1f25f58c40b2170181e8a9871469795e
Noah Overcash <smileytechguy@smileytechguy.com>
2017-01-04 12:01:37 -0500

    Web version finished

--- 15_puzzle/web/15_puzzle.js
+++ 15_puzzle/web/15_puzzle.js
@@ -35,27 +35,74 @@
 
 })(jQuery);
 
-function shuffle(r){for(var f,n,o=r.length;0!==o;)n=Math.floor(Math.random()*o),o-=1,f=r[o],r[o]=r[n],r[n]=f;return r}
+function shuffle(r){for(var f,n,o=r.length;0!==o;)n=Math.floor(Math.random()*o),o-=1,f=r[o],r[o]=r[n],r[n]=f;return r};
 
 $(document).ready(function() {
 	$("td").fitText(0.25);
-})
+	init();
+});
 
-// create array 0-15
-var nums = Array.apply(null, Array(16)).map(function (_, i) {return i;});
+var nums;
 
-// shuffle
-nums = shuffle(nums);
+// holds where the tile is
+// [index, row, col, obj]
+var blankTile;
 
+// put `nums` into table
 function refreshTable() {
 	for (var i = 0; i < $("td").length; i++) {
-		console.log(i);
 		$($("td")[i]).text(nums[i]);
 		if (nums[i] == 0) {
-			$($("td")[i]).css({"backgroundColor": "#fff"});
+			$($("td")[i]).css({"backgroundColor": "#fff"}).text("");
+			blankTile = [i, Math.floor(i/4)+1, i%4+1, $($("td")[i])]
 		} else {
 			$($("td")[i]).css({"backgroundColor": "#ddd"});
 		}
 	}
+};
+
+function init() {
+	// create array 0-15
+	nums = Array.apply(null, Array(16)).map(function (_, i) {return i;});
+
+	// shuffle
+	// nums = shuffle(nums);
+	refreshTable();
 }
-refreshTable();
+
+
+function swapWithBlank(obj) {
+		// swap
+		nums[blankTile[0]] = nums[parseInt($(obj).attr("index"))];
+		$(blankTile[3]).text(nums[blankTile[0]]).css({"backgroundColor": "#ddd"});
+		nums[parseInt($(obj).attr("index"))] = 0;
+		$(obj).text("").css({"backgroundColor": "#fff"});
+		
+		// update blankTile
+		blankTile = [parseInt($(obj).attr("index")), parseInt($(obj).attr("row")), parseInt($(obj).attr("col")), $(obj)];
+}
+
+function checkWinner() {
+	for (var i = 0; i < nums.length-1; i++) {
+		if (nums[i] != i+1) {
+			return false;
+		}
+	}
+	alert("You win!");
+	init();
+	return true;
+}
+
+$("td").click(function() {
+	if (parseInt($(this).attr("row")) == blankTile[1] && parseInt($(this).attr("col")) == blankTile[2]-1) { // left
+		swapWithBlank(this);
+	} else if (parseInt($(this).attr("row")) == blankTile[1] && parseInt($(this).attr("col")) == blankTile[2]+1) { // right
+		swapWithBlank(this);
+		checkWinner();
+	} else if (parseInt($(this).attr("col")) == blankTile[2] && parseInt($(this).attr("row")) == blankTile[1]-1) { // up
+		swapWithBlank(this);
+	} else if (parseInt($(this).attr("col")) == blankTile[2] && parseInt($(this).attr("row")) == blankTile[1]+1) { // down
+		swapWithBlank(this);
+		checkWinner();
+	}
+});
