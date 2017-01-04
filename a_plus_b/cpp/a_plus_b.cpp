a832ea2849fb91b96e0e7d838093b80957035efc
Noah Overcash <smileytechguy@smileytechguy.com>
2017-01-04 18:44:39 -0500

    a plus b: c++ version

---
+++ a_plus_b/cpp/a_plus_b.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<array>
+#include<numeric>
+#include<algorithm>
+#include<functional>
+#include<iterator>
+
+int main() {
+    constexpr int numbers_count = 2;
+    std::array<int,numbers_count> numbers;
+    std::copy_n(std::istream_iterator<int>(std::cin), numbers_count, std::begin(numbers));
+    std::cout<< std::accumulate(numbers.begin(),numbers.end(),0,std::plus<int>())<<std::endl;
+}
