3035d5de8c215e7563467cfc6df14aced079e5f6
Noah Overcash <smileytechguy@smileytechguy.com>
2017-01-27 08:52:41 -0500

    Remove python 3 implementations
    It is practically the same as the python 3 implementation

--- a_plus_b/python3/a_plus_b.py
+++
@@ -1,7 +0,0 @@
-def a_plus_b():
-	uinput = input("Input 2 numbers seperated by spaces: ")
-	exploded = uinput.split()
-	return int(exploded[0])+int(exploded[1])
-
-if __name__ == "__main__":
-	print(a_plus_b())
