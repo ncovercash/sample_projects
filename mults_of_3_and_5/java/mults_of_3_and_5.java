e5c1dc8a45b7618cdbcadb5c54e0ff01f206dd1b
Noah Overcash <smileytechguy@smileytechguy.com>
2017-01-27 14:58:00 -0500

    mults of 3 and 5: java

---
+++ mults_of_3_and_5/java/mults_of_3_and_5.java
@@ -0,0 +1,21 @@
+public class mults_of_3_and_5 {
+	public static void main(String[] args) {
+		System.out.println(get3Sum(Integer.parseInt(args[0]))+get5Sum(Integer.parseInt(args[0])));
+	}
+
+	private static int get3Sum(int max) {
+		int total=0;
+		for (int i=0; i<max; i+=3) {
+			total += i;
+		}
+		return total;
+	}
+
+	private static int get5Sum(int max) {
+		int total=0;
+		for (int i=0; i<max; i+=5) {
+			total += i;
+		}
+		return total;
+	}
+}
