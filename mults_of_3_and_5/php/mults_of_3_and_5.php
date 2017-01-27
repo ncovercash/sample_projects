5a920f383e4f98584ccc39391e464d3bf546ec6c
Noah Overcash <smileytechguy@smileytechguy.com>
2017-01-27 14:23:15 -0500

    mults of 3 and 5: php

---
+++ mults_of_3_and_5/php/mults_of_3_and_5.php
@@ -0,0 +1,23 @@
+<?php
+
+class Mults_of_3_and_5 {
+	public static function getSum($max) {
+		return self::get3Sum($max) + self::get5Sum($max);
+	}
+	private static function get3Sum($max) {
+		$sum=0;
+		for ($i=0; $i < $max; $i+=3) { 
+			$sum+=$i;
+		}
+		return $sum;
+	}
+	private static function get5Sum($max) {
+		$sum=0;
+		for ($i=0; $i < $max; $i+=5) { 
+			$sum+=$i;
+		}
+		return $sum;
+	}
+}
+
+var_dump(Mults_of_3_and_5::getSum(1000));
