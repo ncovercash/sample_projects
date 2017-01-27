2022245c9172037bf71be29d0da53f1c47f6aac9
Noah Overcash <smileytechguy@smileytechguy.com>
2017-01-27 08:50:12 -0500

    guess user number: python3

---
+++ guess_user_number/python3/guess_user_number.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+min=1
+max=100
+
+print("Think of a number from "+str(min)+" to "+str(max)+".  Then, tell the computer if its guess is correct (Y), lower than the number (L), or higher than the number (H).")
+
+found=False
+
+while not found:
+	mid = int((max-min)/2+min)
+	response = input(str(mid)+"? >>> ")
+	response = response.upper()
+	if response == "Y":
+		print("found it!!")
+		found=True
+	elif response == "L":
+		min=mid
+	elif response == "H":
+		max=mid
