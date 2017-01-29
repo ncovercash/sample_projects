65cf0ecd21847a43355f73c093801c2dc60b6912
Noah Overcash <smileytechguy@smileytechguy.com>
2017-01-29 15:51:55 -0500

    special pythagorean triplet: php

---
+++ special_pythagorean_triplet/php/special_pythagorean_triplet.php
@@ -0,0 +1,16 @@
+<?php
+
+$goalSum=1000;
+
+for ($a=1; $a < $goalSum/3; $a++) { 
+	for ($b=$a+1; $b < $goalSum/2; $b++) { 
+		$c = $goalSum-$a-$b;
+		if ($c > 0 && (($a*$a) + ($b*$b)) == ($c*$c)) {
+			if ($a+$b+$c == $goalSum) {
+				$product=$a*$b*$c;
+				echo "a={$a} b={$b} c={$c} product={$product}";
+			}
+		}
+	}
+}
+
