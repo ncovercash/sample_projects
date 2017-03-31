b6fc9f0b3b6fe443d3441d4962fed41f866bc439
Noah Overcash <smileytechguy@smileytechguy.com>
2017-03-31 09:45:28 -0400

    lychrel nums: add comments

--- lychrel_nums/php/lychrel_nums.php
+++ lychrel_nums/php/lychrel_nums.php
@@ -1,27 +1,43 @@
 <?php
 
-function isBasicPalandronic(string $in) : bool {
-	return $in == strrev($in);
+/**
+ * bool isBasicPalindronic(string $in)
+ *
+ * @param string $in input to be tested for palindrinity
+ *          must be a number or string with no spaces
+ * @return boolean true if the string is a palindrome
+ */
+function isBasicPalindronic(string $in) : bool {
+	return $in == strrev($in);	// spaces and such will not work with this
+								// numbers only
 }
 
+/**
+ * bool isLychrelNumber(int $in)
+ *   Only computes 50 iterations
+ *
+ * @param int $in input to be tested for lychrel's theory
+ * @return boolean true if the number is a lychrel number
+ */
 function isLychrelNumber(int $in) : bool {
-	$num = gmp_init($in);
+	$num = gmp_init($in); // use GMP as these numbers could overflow
 	$count = 1;
-	do {
+	do { // perform initial required iteration before testing for palindroem
 		$reversedString = strrev(gmp_strval($num));
-		while ($reversedString[0] == "0") {
+		while ($reversedString[0] == "0") { // check for leading zeroes, as GMP will not work with them
 			$reversedString = substr($reversedString, 1);
 		}
 		$reversedNum = gmp_init($reversedString);
 		$num = gmp_add($num, $reversedNum);
 		$count++;
-	} while (!isBasicPalandronic(gmp_strval($num)) && $count < 50);
-	return !isBasicPalandronic(gmp_strval($num));
+	} while (!isBasicPalindronic(gmp_strval($num)) && $count < 50);
+
+	return !isBasicPalindronic(gmp_strval($num));
 }
 
 $numLychrel = 0;
 
-for ($i=0; $i < 10000; $i++) { // we can start at 10 as all basic digits are palindronic
+for ($i=1; $i < 10000; $i++) {
 	$numLychrel += isLychrelNumber($i);
 }
 
