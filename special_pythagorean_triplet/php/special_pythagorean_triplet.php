5a737d064334fec1072726ae032c12e449bb7adf
Noah Overcash <smileytechguy@smileytechguy.com>
2017-04-13 23:48:28 -0400

    Special Pythagorean Triplet: time

--- special_pythagorean_triplet/php/special_pythagorean_triplet.php
+++ special_pythagorean_triplet/php/special_pythagorean_triplet.php
@@ -2,13 +2,17 @@
 
 $goalSum=1000;
 
+$start = microtime(true);
+
 for ($a=1; $a < $goalSum/3; $a++) { 
 	for ($b=$a+1; $b < $goalSum/2; $b++) { 
 		$c = $goalSum-$a-$b;
 		if ($c > 0 && (($a*$a) + ($b*$b)) == ($c*$c)) {
 			if ($a+$b+$c == $goalSum) {
 				$product=$a*$b*$c;
-				echo "a={$a} b={$b} c={$c} product={$product}";
+				echo "a={$a} b={$b} c={$c} product={$product}\n";
+				echo "Took ".(microtime(true)-$start)." seconds";
+				die();
 			}
 		}
 	}
